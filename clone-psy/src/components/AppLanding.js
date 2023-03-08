d2ecfadc5ac53ebfd0d22a50c4608dfe042ad86e
Sorn Tangmettri <123283256+Sorn4web@users.noreply.github.com>
2023-03-08 17:28:24 +0700

    finished landing page

---
+++ clone-psy/src/components/AppLanding.js
@@ -0,0 +1,23 @@
+import "./Applanding.css";
+
+function AppLanding() {
+  return (
+    <div className="container">
+      <div className="header">
+        <h1>ทุกข์ใจอยู่ไหม?</h1>
+      </div>
+      <div className="subHeader">
+        <p>
+          หากคุณกำลังต้องการความช่วยเหลือ <br />
+          ได้โปรดรู้ไว้ว่ามีคนมากมายที่พร้อมจะรับฟังคุณ
+        </p>
+      </div>
+      <div className="button">
+        <button className="but1">ช่วยฉันที !!</button>
+        <button className="but2">ฉันไม่เป็นไร พาฉันเข้าสู่เว็บไซต์</button>
+      </div>
+    </div>
+  );
+}
+
+export default AppLanding;
